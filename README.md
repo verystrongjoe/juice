68e92b28f6dcd92bd0c29efcce62efda641f0e18
JoUk <verystrongjoe@gmail.com>
2017-03-09 09:36:31 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Juice Project
+
+## Alogorithm
+
+## Index
+ -  Resilience
+  . 회복 탄력성 지수를 구하는 모듈
---
+++ run.py
@@ -0,0 +1,9 @@
+from Index import Resilience
+
+p = Resilience.Resilience()
+
+#p.main('005930') #삼성전자
+#p.main('018260') #삼성에스디에스
+p.main('004020') #현대제철
+
+
